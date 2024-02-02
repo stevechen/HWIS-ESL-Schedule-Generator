c500d2da25ddfa577f7513ca8660ff55fe1b6b6f
stevechen <steve.stevechen@gmail.com>
2024-02-02 09:30:34 +0900

    initial svelte project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import moment from 'moment';
+
+	let day1 = moment('2024-01-01');
+	let day2 = moment('2024-01-01');
+	let difference = day1.isSame(day2);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>difference: {difference}</p>
