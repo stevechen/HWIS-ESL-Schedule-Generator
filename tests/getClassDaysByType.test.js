ab33c29e7d05b07e99e8213484d6493555a61d40
stevechen <steve.stevechen@gmail.com>
2024-02-07 09:42:01 +0800

    add test for different grade and types

--- tests/getClassDaysByType.test.js
+++ tests/getClassDaysByType.test.js
@@ -9,41 +9,96 @@ it('get all classes of class days for CLIL', () => {
   const allClassDays = getDates(startDate, endDate, specialDays);
   const classDays = getClassDaysByType(allClassDays, [1, 3, 5], 'CLIL');
   // days should not have any days other than indicated weekdays
-  let numberOfOtherDays = 0;
+  let otherDaysCount = classDays.filter(day => day.weekday===2 || day.weekday===4).length;
 
-  classDays.forEach(day => {
-    if (day.weekday===2 || day.weekday ===4) numberOfOtherDays++;
-  });
-
-  expect(numberOfOtherDays).toBe(0);
+  expect(otherDaysCount).toBe(0);
 });
 
 it('get all classes of class days for Comm', () => {
   let startDate = '2023-08-30';
   let endDate = '2024-01-19'
   const allClassDays = getDates(startDate, endDate, specialDays);
-  const classDays = getClassDaysByType(allClassDays, [2, 4], 'Comm');
+  const classDays = getClassDaysByType(allClassDays, [2, 4],'Comm');
   // days should not have any days other than indicated weekdays
-  let numberOfOtherDays = 0;
-
-  classDays.forEach(day => {
-    if (day.weekday===1 || day.weekday ===3 || day.weekday ===5) numberOfOtherDays++;
-  });
-
-  expect(numberOfOtherDays).toBe(0);
+  let otherDaysCount = classDays.filter(day => day.weekday === 3 || day.weekday === 3 || day.weekday === 5).length;
+  
+  expect(otherDaysCount).toBe(0);
 });
 
-it('Comm class Oral exam days inserted', () => {
+it('G7 Comm class 6 Oral exam days inserted', () => {
   let startDate = '2023-08-30';
   let endDate = '2024-01-19'
   const allClassDays = getDates(startDate, endDate, specialDays);
-  const classDays = getClassDaysByType(allClassDays, [2, 4], 'Comm');
-  // days should not have any days other than indicated weekdays
-  let numberOfOralExamDays = 0;
+  let classDays = getClassDaysByType(allClassDays, [2, 4], 'Comm', 'G7');
+   // 6 oral exams, 2 before each exam
+  let oralExamDaysCount = classDays.filter(day => day.description === 'Oral Exam').length;
 
-  classDays.forEach(day => {
-    if (day.description==='Oral Exam') numberOfOralExamDays++;
-  });
-
-  expect(numberOfOralExamDays).toBe(6);
+  expect(oralExamDaysCount).toBe(6);
 });
+
+it('G8 Comm class 6 Oral exam days inserted', () => {
+  let startDate = '2023-08-30';
+  let endDate = '2024-01-19'
+  const allClassDays = getDates(startDate, endDate, specialDays);
+  let classDays = getClassDaysByType(allClassDays, [2, 4], 'Comm', 'G8');
+   // 6 oral exams, 2 before each exam
+  let oralExamDaysCount = classDays.filter(day => day.description ==='Oral Exam').length;
+
+  expect(oralExamDaysCount).toBe(6);
+});
+
+it('G9 Comm class 6 Oral exam days inserted', () => {
+  let startDate = '2023-08-30';
+  let endDate = '2024-01-19'
+  const allClassDays = getDates(startDate, endDate, specialDays);
+  let classDays = getClassDaysByType(allClassDays, [1, 3], '', 'G9');
+   // 6 oral exams, 2 before each exam
+  let oralExamDaysCount = classDays.filter(day => day.description === 'Oral Exam').length;
+
+  expect(oralExamDaysCount).toBe(6);
+});
+
+it('H10 Comm class 2 Oral exam days inserted', () => {
+  let startDate = '2023-08-30';
+  let endDate = '2024-01-19'
+  const allClassDays = getDates(startDate, endDate, specialDays);
+  let classDays = getClassDaysByType(allClassDays, [2, 4], '', 'H10');
+   // no oral exams except before the final exam
+  let oralExamDaysCount = classDays.filter(day => day.description === 'Oral Exam').length;
+
+  expect(oralExamDaysCount).toBe(2);
+});
+
+it('H11 Comm class 2 Oral exam days inserted', () => {
+  let startDate = '2023-08-30';
+  let endDate = '2024-01-19'
+  const allClassDays = getDates(startDate, endDate, specialDays);
+  let classDays = getClassDaysByType(allClassDays, [2, 4], '', 'H11');
+  // no oral exams except before the final exam
+  let oralExamDaysCount = classDays.filter(day => day.description === 'Oral Exam').length;
+
+  expect(oralExamDaysCount).toBe(2);
+});
+
+it('G7 CLIL class has no Oral exam days inserted', () => {
+  let startDate = '2023-08-30';
+  let endDate = '2024-01-19'
+  const allClassDays = getDates(startDate, endDate, specialDays);
+  let classDays = getClassDaysByType(allClassDays, [1, 3, 5], 'CLIL', 'G7');
+  // no oral exams
+  let oralExamDaysCount = classDays.filter(day => day.description === 'Oral Exam').length;
+
+  expect(oralExamDaysCount).toBe(0);
+});
+
+it('G8 CLIL class has no Oral exam days inserted', () => {
+  let startDate = '2023-08-30';
+  let endDate = '2024-01-19'
+  const allClassDays = getDates(startDate, endDate, specialDays);
+  let classDays = getClassDaysByType(allClassDays, [2, 4, 5], 'CLIL', 'G8');
+  // no oral exams
+  let oralExamDaysCount = classDays.filter(day => day.description === 'Oral Exam').length;
+
+  expect(oralExamDaysCount).toBe(0);
+});
+
