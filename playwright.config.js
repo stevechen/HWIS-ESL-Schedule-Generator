040df04d2fc7d4391775358488499d18a4d05e35
stevechen <steve.stevechen@gmail.com>
2024-03-28 23:04:46 +0800

    set up playwright

--- playwright.config.js
+++ playwright.config.js
@@ -2,9 +2,10 @@
 const config = {
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 4173
+		port: 4173,
+		reuseExistingServer: true
 	},
-	testDir: 'tests',
+	testDir: 'tests-e2e',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/
 };
 
