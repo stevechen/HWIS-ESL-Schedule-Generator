a8730115a553fd7e5ac797b27452eea2e6bd0ea9
stevechen <steve.stevechen@gmail.com>
2024-03-30 18:05:44 +0800

    add more mock data sets remove grade radio checkbox test (layout change) re-name classes change

--- tests-e2e/test.js
+++ tests-e2e/test.js
@@ -4,11 +4,11 @@ const BASE_URL = 'http://localhost:5173';
 const MOCK_STUDENT_DATA = `1234567\t張三\tSan Chang\tJ101
 7654321\t李四\tSi Li\tJ102`;
 
+const MOCK_STUDENT_DATA_G9 = `1234567\t張三\tSan Chang\tJ301
+7654321\t李四\tSi Li\tJ302`;
+
 function initializeLocators(page) {
   return {
-    locatorRadioG7: page.locator('input[type="radio"][value="G7"]'),
-    locatorRadioG8: page.locator('input[type="radio"][value="G8"]'),
-    locatorRadioG9: page.locator('input[type="radio"][value="G9"]'),
     locatorRadioCLIL: page.locator('input[type="radio"][value="CLIL"]'),
     locatorRadioComm: page.locator('input[type="radio"][value="Comm"]'),
     locatorRadioPassport: page.locator('input[type="radio"][value="passport"]'),
@@ -21,13 +21,10 @@ function initializeLocators(page) {
 async function pasteDataIntoInput(page, context, selector, mockData) {
     // focus on the input
   await page.locator(selector).focus();
-
   // grant access to clipboard (you can also set this in the playwright.config.ts file)
   await context.grantPermissions(['clipboard-read', 'clipboard-write']);
-
   // Set the clipboard content to the desired data
   await page.evaluate((data) => navigator.clipboard.writeText(data), mockData);
-
   // simulate paste event
   await page.keyboard.press(`${modifier}+V`);
 }
@@ -38,38 +35,15 @@ const isMac = async () => 'userAgentData' in navigator ?
 
 let modifier = isMac ? 'Meta' : 'Control';
 
-test('should allow grade selection and update class type for G9', async ({ page }) => {
-  // Navigate to your Svelte application page
-  await page.goto(`${BASE_URL}/commslip`);
-  const { locatorRadioG7, locatorRadioG8, locatorRadioCLIL, locatorRadioComm } = initializeLocators(page);
-
-  // Select grade G8 and check if selected correctly
-  await page.click('input[type="radio"][value="G8"]');
-  await expect(locatorRadioG8).toBeChecked();
-
-  // Select grade G9 and verify class type switches to 'Comm'
-  await page.click('input[type="radio"][value="G9"]');
-  await expect(locatorRadioComm).toBeChecked();
-  await expect(locatorRadioCLIL).toBeHidden();
-
-  // Select grade G7 and check if selected correctly and CLIL is shown
-  await page.click('input[type="radio"][value="G7"]');
-  await expect(locatorRadioG7).toBeChecked();
-  await expect(locatorRadioCLIL).toBeVisible();
-});
-
-test('should hide assignment type for different classes', async({ page }) => {
+test('should hide assignment type for different classes', async({ page, context }) => {
   await page.goto(`${BASE_URL}/commslip`);
   const { locatorRadioPassport, locatorRadioRecording, locatorRadioWorkbook, locatorRadioExam } = initializeLocators(page);
 
-  await page.click('input[type="radio"][value="G7"]');
-  await page.click('input[type="radio"][value="CLIL"]');
-  await expect(locatorRadioPassport).toBeHidden();
-  await expect(locatorRadioRecording).toBeHidden();
-  await expect(locatorRadioExam).toBeHidden();
-  await expect(locatorRadioWorkbook).toBeVisible();
+  await pasteDataIntoInput(page, context, '#sList', MOCK_STUDENT_DATA_G9);
 
-  await page.click('input[type="radio"][value="G9"]');
+  await expect(locatorRadioPassport).toBeVisible();
+  await expect(locatorRadioRecording).toBeVisible();
+  await expect(locatorRadioExam).toBeVisible();
   await expect(locatorRadioWorkbook).toBeHidden();
 });
 
@@ -162,17 +136,18 @@ test('should update slip fields with manual data change', async ({ page, context
 
   await pasteDataIntoInput(page, context, '#sList', MOCK_STUDENT_DATA);
 
-  await page.fill('tr:nth-child(2) .id', '5555555');
-  await expect(page.locator('.slip:nth-child(2) .id')).toContainText("5555555");
+  //change second row data
+  await page.fill('tr:nth-child(2) .student-id', '5555555');
+  await expect(page.locator('.slip:nth-child(2) .student-id')).toContainText("5555555");
 
-  await page.fill('tr:nth-child(2) .chinese_name', '王八');
-  await expect(page.locator('.slip:nth-child(2) .chinese_name')).toContainText("王八");
+  await page.fill('tr:nth-child(2) .chinese-name', '王八');
+  await expect(page.locator('.slip:nth-child(2) .chinese-name')).toContainText("王八");
 
-  await page.fill('tr:nth-child(2) .english_name', 'Mary Jane');
-  await expect(page.locator('.slip:nth-child(2) .english_name')).toContainText("Mary Jane");
+  await page.fill('tr:nth-child(2) .english-name', 'Mary Jane');
+  await expect(page.locator('.slip:nth-child(2) .english-name')).toContainText("Mary Jane");
 
-  await page.fill('tr:nth-child(2) .chinese_class', 'J112');
-  await expect(page.locator('.slip:nth-child(2) .chinese_class')).toContainText("J112");
+  await page.fill('tr:nth-child(2) .chinese-class', 'J112');
+  await expect(page.locator('.slip:nth-child(2) .chinese-class')).toContainText("J112");
 
   await page.click('tr:nth-child(2) > td > select');
   await page.selectOption('tr:nth-child(2) > td > select', { value: "1" });
