20e7df932a3b3475e8e612f0b04a280551b6181c
stevechen <steve.stevechen@gmail.com>
2024-04-17 00:58:42 +0800

    - rearrange code

--- src/lib/stores/communication.svelte.ts
+++ src/lib/stores/communication.svelte.ts
@@ -1,34 +1,19 @@
 import { writable } from "svelte/store";
 
+// #region Constants
 export const STATUS_TYPE = [
   { code: 0, text: { english: "hasn't been submitted", chinese: '未繳交' } },
   { code: 1, text: { english: "wasn't completed", chinese: '完成度不佳' } }
 ];
 
-export let assignment = writable({
+export const assignment = writable({
 	esl: '',
 	type: {code: '', english: '', chinese: ''},
 	assigned: '',
 	due: '',
 	late: ''
 });
-
-export function createAssignment() {
-  let detail = $state({
-    esl: '',
-    type: {code: '', english: '', chinese: ''},
-    assigned: '',
-    due: '',
-    late: '',
-  });
-  return detail;
-}
-
-export function isValidDate(dateStr: string | null) { 
-	const REGEX = /^((0?[13578]|1[02])\/(0?[1-9]|[12][0-9]|3[01])|(0?[469]|11)\/(0?[1-9]|[12][0-9]|30)|(0?2)\/(0?[1-9]|1[0-9]|2[0-9]))$/;
-	return dateStr === null ? false : REGEX.test(dateStr);
-}
-
+// #region Types
 export interface Student {
   id: string;
   name: {
@@ -50,4 +35,19 @@ export interface Assignment {
   due: string | null;
   late: string | null;
 }
+// #region Functions
+export function createAssignment() {
+  let detail = $state({
+    esl: '',
+    type: {code: '', english: '', chinese: ''},
+    assigned: '',
+    due: '',
+    late: '',
+  });
+  return detail;
+}
 
+export function isValidDate(dateStr: string | null) { 
+	const REGEX = /^((0?[13578]|1[02])\/(0?[1-9]|[12][0-9]|3[01])|(0?[469]|11)\/(0?[1-9]|[12][0-9]|30)|(0?2)\/(0?[1-9]|1[0-9]|2[0-9]))$/;
+	return dateStr === null ? false : REGEX.test(dateStr);
+}
