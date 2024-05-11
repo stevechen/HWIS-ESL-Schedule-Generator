825156df5af0c9bf8a548edbb71b75070dc1da9a
stevechen <steve.stevechen@gmail.com>
2024-05-11 14:15:03 +0800

    - remove and incorporate Input component into the scheduler page - move into Svelte 5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,127 @@
-<script>
-	import Input from '$lib/components/Input.svelte';
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { getDates } from '$lib/getAllClassDays.ts.svelte';
+	import { getClassDaysByType } from '$lib/getClassDaysByType.ts.svelte';
 	import TabBar from '$lib/components/TabBar.svelte';
 
-	let generatedDatesOutput = '';
-	/**
-	 * Handles the 'generate' event from the Input component.
-	 * @param {CustomEvent} event - The event object from the Input component.
-	 */
-	function handleMessage(event) {
-		generatedDatesOutput = event.detail.generatedDatesOutput;
+	enum ClassTypeCode {
+		Comm = 'Comm',
+		CLIL = 'CLIL',
+		G9 = 'G9',
+		H = 'H'
+	}
+
+	const classControl = [
+		{ code: ClassTypeCode.CLIL, key: 'CLIL', label: 'G7/8 CLIL' },
+		{ code: ClassTypeCode.Comm, key: 'Comm', label: 'G7/8 Comm' },
+		{ code: ClassTypeCode.G9, key: 'G9', label: 'G9' },
+		{ code: ClassTypeCode.H, key: 'H', label: 'H10/11' }
+	];
+
+	let UIStateClassType = $state(ClassTypeCode.CLIL); //default
+
+	const grade = $derived.by(() => {
+		if (UIStateClassType === ClassTypeCode.CLIL) {
+			return 'G7/8';
+		} else if (UIStateClassType === ClassTypeCode.Comm) {
+			return 'G7/8';
+		} else if (UIStateClassType === ClassTypeCode.G9) {
+			return 'G9';
+		} else if (UIStateClassType === ClassTypeCode.H) {
+			return 'H';
+		}
+	});
+
+	const WEEKDAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
+	let UIStateCheckedDays = $state([true, false, true, false, true]); //default
+
+	let UIStateEventsText = $state('');
+
+	let checkedDays = $derived(
+		UIStateCheckedDays.map((isChecked, index) => (isChecked ? index + 1 : null)) //return day number if the checkbox is checked
+			.filter((index): index is number => index !== null) // filter out null values created by unchecked checkboxes)
+	);
+
+	let UIStateOutput = $state('');
+
+	$effect(() => {
+		const allClassDays = getDates(UIStateEventsText);
+		const classDates = getClassDaysByType(allClassDays, checkedDays, UIStateClassType, grade);
+		const generatedDatesOutput = ['#\tDate\tDescription\tNote']
+			.concat(
+				classDates.map((row) => [row.countdown, row.date, row.description, row.note].join('\t'))
+			)
+			.join('\n');
+		UIStateOutput = generatedDatesOutput;
+	});
+
+	onMount(async () => {
+		const CUT_OFF_MONTH = 7;
+		// if we are close to semester 2, load the semester 2 events data
+		const yearAndSemester =
+			new Date().getMonth() < CUT_OFF_MONTH
+				? `${new Date().getFullYear() - 1}-${new Date().getFullYear()}-2`
+				: `${new Date().getFullYear()}-${new Date().getFullYear() + 1}-1`;
+		UIStateEventsText =
+			(await loadSchoolEvents(yearAndSemester)) || (await loadSchoolEvents('2023-2024-1'));
+	});
+
+	async function loadSchoolEvents(fileNamePrefix: string) {
+		try {
+			const module = await import(`$lib/data/${fileNamePrefix}-schoolEvents.js`);
+			return module.schoolEvents
+				.split('\n')
+				.filter((line: string) => line.trim() !== '')
+				.join('\n');
+		} catch (error) {
+			console.error(`Failed to load ${fileNamePrefix}-schoolEvents.js`, error);
+			return null;
+		}
 	}
 </script>
 
 <title>HWIS ESL Tools</title>
 <TabBar />
 <main>
-	<section id="input">
-		<Input on:generate={handleMessage} />
-	</section>
+	<section id="output">
+		<h3>Controls</h3>
+		<div id="options">
+			<div id="weekdays">
+				<div id="types">
+					<h3>Type</h3>
+					{#each classControl as { code, key, label }}
+						<input type="radio" id={key} name={key} bind:group={UIStateClassType} value={code} />
+						<label for={key}>{label}</label>
+					{/each}
 
+					<h3>Days</h3>
+					{#if UIStateCheckedDays.length === 0}
+						<p class="warning">Select at least one day!</p>
+					{/if}
+					{#each UIStateCheckedDays as _, index (index)}
+						<input type="checkbox" id={WEEKDAYS[index]} bind:checked={UIStateCheckedDays[index]} />
+						<label for={WEEKDAYS[index]}>{WEEKDAYS[index]}</label>
+					{/each}
+				</div>
+			</div>
+		</div>
+		<div id="schoolEvents">
+			<h3>
+				Events
+				<button class="info">
+					ℹ️
+					<span class="info_note">
+						Copy a table from spreadsheet with 4 fields: date, description, note, type
+					</span>
+				</button>
+			</h3>
+			<textarea rows="30" bind:value={UIStateEventsText} readonly></textarea>
+		</div>
+	</section>
 	<section id="output">
 		<h3>Generated Dates (Copy to Excel)</h3>
-		<textarea rows="30" bind:value={generatedDatesOutput} readonly></textarea>
+		<!-- <textarea rows="30" bind:value={UIStateOutput} readonly></textarea> -->
+		<textarea rows="30" value={UIStateOutput} readonly></textarea>
 	</section>
 </main>
 
@@ -38,11 +138,6 @@
 		padding-bottom: 1em;
 	}
 
-	h3 {
-		font-size: 0.9em;
-		margin-bottom: 0.3em;
-	}
-
 	#input,
 	#output {
 		display: flex;
@@ -53,5 +148,48 @@
 		min-width: 58em;
 		flex: 1;
 		font-size: 0.8em;
+	}
+
+	h3 {
+		font-size: 0.9em;
+		margin-bottom: 0.3em;
+	}
+
+	#options {
+		display: flex;
+		flex-direction: column;
+		border: 1px solid gray;
+		padding: 0 0.5em 0.5em 0.5em;
+		border-radius: 10px;
+	}
+
+	button.info {
+		border: none;
+		background: none;
+	}
+	.info_note {
+		display: none;
+	}
+
+	button.info:hover .info_note {
+		display: inline;
+		font-size: 0.8em;
+	}
+
+	#schoolEvents {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		row-gap: 5px;
+	}
+
+	#events {
+		min-width: 50em;
+		flex: 1;
+		font-size: 0.8em;
+	}
+
+	.warning {
+		color: red;
 	}
 </style>
