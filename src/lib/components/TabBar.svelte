d5d791b43f5224c4179325b5f82d62dc817f1cc6
stevechen <steve.stevechen@gmail.com>
2025-01-08 07:06:21 +0800

    • add and adopt to tailwindcss • Fixed printing issue caused by tailwindcss base font size change • Modify class info selector style • Brought back Print button since the printing issue seems to be fixed… • Add Place holder for ESL class number • packages update

--- src/lib/components/TabBar.svelte
+++ src/lib/components/TabBar.svelte
@@ -7,14 +7,18 @@
 	let currentPage = $state(get(page).url.pathname);
 
 	const NAV_ITEM = [
-		{ path: '/', name: 'Schedule' },
-		{ path: '/communication', name: 'Communication Slip' }
+		{ name: 'Schedule', url: '/' },
+		{ name: 'Communication Slip', url: '/communication' }
 	];
 </script>
 
-<nav class="tab-bar no-print">
-	{#each NAV_ITEM as nav}
-		<a href={nav.path} class:active={currentPage === nav.path}>{nav.name}</a>
+<nav class="tab-bar no-print flex justify-center bg-slate-200">
+	{#each NAV_ITEM as { url, name }}
+		<a
+			class="rounded-t-lg py-2 px-6 text-decoration-none font-bold text-slate-500"
+			href={url}
+			class:active={currentPage === url}>{name}</a
+		>
 	{/each}
 </nav>
 
@@ -28,16 +32,7 @@
 		}
 	}
 	.tab-bar {
-		display: flex;
-		justify-content: center;
-		background-color: #f0f0f0;
 		margin-bottom: 3px;
-	}
-	.tab-bar a {
-		text-decoration: none;
-		color: darkgray;
-		font-weight: bold;
-		padding: 0.5em 2em;
 	}
 
 	.tab-bar a:not(.active):hover {
