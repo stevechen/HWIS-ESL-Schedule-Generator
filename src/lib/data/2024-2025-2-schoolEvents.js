99b3484b2df6253170478c58d21e73140dbd766e
stevechen <steve.stevechen@gmail.com>
2025-01-22 09:13:40 +0800

    add 2023-2024 calendar draft file

---
+++ src/lib/data/2024-2025-2-schoolEvents.js
@@ -0,0 +1,21 @@
+/** @type {String} */
+export const schoolEvents = 
+`
+2025-02-11	Back to school. Starts on P3		
+2025-02-28	Off	Peace day	
+2025-03-25	Exam		
+2025-03-26	Exam		
+2025-04-04	Off	Children's day	
+2025-04-17	Off Mock Exam		G9
+2025-04-18	Off Mock Exam		G9
+2025-04-27	Off	sports day	
+2025-04-28	Off	for 4/27 sports day	
+2025-05-14	Exam		
+2025-05-15	Exam		
+2025-05-16	Only morning classes		G9
+2025-05-30	Off	Dragon Boat Festival	
+2025-06-02	Off	Graduation Ceremony	G9
+2025-06-26	Exam		
+2025-06-27	Exam		
+2025-06-30	Exam		
+`;
