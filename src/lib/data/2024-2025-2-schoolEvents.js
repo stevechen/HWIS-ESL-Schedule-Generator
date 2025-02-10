e2a9e911b2b01b9e75731bea1f8227c84914d513
stevechen <steve.stevechen@gmail.com>
2025-02-10 10:28:03 +0800

    Update 24-25 School Events

--- src/lib/data/2024-2025-2-schoolEvents.js
+++ src/lib/data/2024-2025-2-schoolEvents.js
@@ -2,19 +2,31 @@
 export const schoolEvents = 
 `
 2025-02-11	Back to school. Starts on P3		
+2025-02-19	Off	G9 Mock Exam	G9
+2025-02-20	Off	G9 Mock Exam	G9
 2025-02-28	Off	Peace day	
 2025-03-25	Exam		
 2025-03-26	Exam		
-2025-04-04	Off	Children's day	
-2025-04-17	Off Mock Exam		G9
-2025-04-18	Off Mock Exam		G9
-2025-04-27	Off	sports day	
+2025-04-03	Off	Spring break	
+2025-04-04	Off	Spring break	
+2025-04-17	Off	G9 Mock Exam	G9
+2025-04-18	Off	G9 Mock Exam	G9
+2025-04-26	Off	School Anniversary	
 2025-04-28	Off	for 4/27 sports day	
 2025-05-14	Exam		
 2025-05-15	Exam		
 2025-05-16	Only morning classes		G9
 2025-05-30	Off	Dragon Boat Festival	
 2025-06-02	Off	Graduation Ceremony	G9
+2025-06-03		Passport Check	Comm
+2025-06-04		Passport Check	Comm
+2025-06-05		Passport Check	Comm
+2025-06-06		Passport Check	Comm
+2025-06-07		Passport Check	Comm
+2025-06-11		Workbook Check	CLIL
+2025-06-12		Workbook Check	CLIL
+2025-06-13		Workbook Check	CLIL
+2025-06-14		Workbook Check	CLIL
 2025-06-26	Exam		
 2025-06-27	Exam		
 2025-06-30	Exam		
