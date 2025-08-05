5206724bf39dd46c48df40dfab96149cc9deec16
stevechen <steve.stevechen@gmail.com>
2025-08-05 13:55:17 +0800

    School Events correction

--- src/lib/data/2025-2026-1-schoolEvents.js
+++ src/lib/data/2025-2026-1-schoolEvents.js
@@ -5,6 +5,7 @@ export const schoolEvents =
 2026-01-15	Exam		
 2026-01-14	Exam		
 2026-01-01	Off	New Year	
+2025-12-25	Off	Constitution Day	
 2025-12-24	G9 Mock Exam		G9
 2025-12-23	G9 Mock Exam		G9
 2025-12-29		CLIL WB Check	CLIL
@@ -21,12 +22,13 @@ export const schoolEvents =
 2025-12-04	Exam		
 2025-11-07	Sport's Day		
 2025-10-29	Sports day rehearsal		
-2025-10-24	Off	Taiwan Liberation Day	
+2025-10-24	Off	Taiwan Liberation Day make up	
 2025-10-15	Exam		
 2025-10-14	Exam		
 2025-10-10	Off	Double Ten	
+2025-10-06	Off	Moon Festival	
 2025-10-01	BBQ		
-2025-08-29	Off	Teacher's Day	
+2025-09-29	Off	Teacher's Day make up	
 2025-09-09	G9 Mock Exam		G9
 2025-09-10	G9 Mock Exam		G9
 2025-09-01	starts from 3rd period		
