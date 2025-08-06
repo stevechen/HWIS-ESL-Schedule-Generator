5a1d0094076e62f1e5ab1f99492240efc5833d6a
stevechen <steve.stevechen@gmail.com>
2025-08-06 16:53:40 +0800

    • update schedule

--- src/lib/data/2025-2026-1-schoolEvents.js
+++ src/lib/data/2025-2026-1-schoolEvents.js
@@ -20,14 +20,14 @@ export const schoolEvents =
 2025-12-08		Passport Check	Comm
 2025-12-05	Exam		
 2025-12-04	Exam		
-2025-11-07	Sport's Day		
+2025-11-07	Sport's Day (no class)		
 2025-10-29	Sports day rehearsal		
 2025-10-24	Off	Taiwan Liberation Day make up	
 2025-10-15	Exam		
 2025-10-14	Exam		
 2025-10-10	Off	Double Ten	
 2025-10-06	Off	Moon Festival	
-2025-10-01	BBQ		
+2025-10-01	BBQ (no class)		
 2025-09-29	Off	Teacher's Day make up	
 2025-09-09	G9 Mock Exam		G9
 2025-09-10	G9 Mock Exam		G9
